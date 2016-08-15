54ec994c638bb1d661d42e2f7a17471531b682a2
BuildTools <unconfigured@null.spigotmc.org>
2016-08-15 10:43:45 -0500

    Added Enable and Disable Code Lines
    Added the onEnable and onDisable code lines for the plugin to start-up
    and disable.

--- src/com/craft4plus/main/Main.java
+++ src/com/craft4plus/main/Main.java
@@ -4,6 +4,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
-	//START CODING :D
+	@Override
+	public void onEnable() {
+		System.out.println("Craft4Plus has been enabled!");
+	}
+	
+	@Override
+	public void onDisable() {
+		System.out.println("Craft4Plus has been disabled!");
+	}
 
 }
---
+++ src/com/craft4plus/parties/Parties.java
@@ -0,0 +1,9 @@
+package com.craft4plus.parties;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Parties extends JavaPlugin{
+	
+	public void
+
+}
