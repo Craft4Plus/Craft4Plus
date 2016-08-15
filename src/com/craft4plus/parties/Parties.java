7b53568a082aada15fa9c4589291aeac2b62a108
BuildTools <unconfigured@null.spigotmc.org>
2016-08-15 12:06:55 -0500

    New Party Stuff

--- src/com/craft4plus/parties/Parties.java
+++ src/com/craft4plus/parties/Parties.java
@@ -1,14 +1,20 @@
 package com.craft4plus.parties;
 
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Parties extends JavaPlugin{
 	
 	public boolean onCommand(Command sender, Command cmd, String label, String[] args) {
-		if(sender instanceof player) {
+		if(sender instanceof Player) {
 		Player player = (Player) sender;
-		if(cmd.getName().equalsIgnoreCase("party invite"));
+		if(cmd.getName().equalsIgnoreCase("party invite " + player.getName() )) {
+			
+			((CommandSender) sender).sendMessage(player.getName() + " has been invited to your party");
+			return true;
+		}
 		}
 		return false;
 		
