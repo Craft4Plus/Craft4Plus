24027660d85c7ddbf8ad7a8d1d1edcf267fd50aa
BuildTools <unconfigured@null.spigotmc.org>
2016-08-15 11:12:09 -0500

    Added one command of Parties

--- src/com/craft4plus/parties/Parties.java
+++ src/com/craft4plus/parties/Parties.java
@@ -1,9 +1,17 @@
 package com.craft4plus.parties;
 
+import org.bukkit.command.Command;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Parties extends JavaPlugin{
 	
-	public void
+	public boolean onCommand(Command sender, Command cmd, String label, String[] args) {
+		if(sender instanceof player) {
+		Player player = (Player) sender;
+		if(cmd.getName().equalsIgnoreCase("party invite"));
+		}
+		return false;
+		
+	}
 
 }
