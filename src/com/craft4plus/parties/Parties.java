4128332c98e1c1f900580e649c3210695f624fbf
chrismin13 <chrismin13@gmail.com>
2016-08-17 17:25:24 +0300

    Continued work on Parties

--- src/com/craft4plus/parties/Parties.java
+++ src/com/craft4plus/parties/Parties.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import net.md_5.bungee.api.ChatColor;
 
@@ -11,6 +12,7 @@ public class Parties implements Listener {
 
 	String PartyPrefix = ChatColor.GRAY + "[" + ChatColor.BLUE + "Party" + ChatColor.GRAY + "]" + ChatColor.RESET;
 
+	@EventHandler
 	public boolean onCommand(Command sender, Command cmd, String label, String[] args) {
 		if (sender instanceof Player) {
 			if (cmd.getName().equalsIgnoreCase("party invite " + args[0])) {
@@ -20,10 +22,8 @@ public class Parties implements Listener {
 							.sendMessage(PartyPrefix + serverplayer.getName() + " has been invited to your party.");
 					return true;
 				}
-				((CommandSender) sender).sendMessage(PartyPrefix + "Cannot find a player by that username.");
-				return false;
 			}
-
+			((CommandSender) sender).sendMessage(PartyPrefix + "Cannot find a player by that username.");
 			return false;
 		}
 		return false;
