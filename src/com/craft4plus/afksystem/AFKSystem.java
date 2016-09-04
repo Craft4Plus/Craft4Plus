51d355ac25c28a9bc4e639bc293a129ceae303c2
UnitedGamersMC <logangaither67@gmail.com>
2016-09-04 14:38:31 -0500

    Added AFKSystem Class
    I wanted to add an AFK system similar to Hypixel so staff and players
    have a small private zone to be in, while either working without riots
    of players under them, or for players to idle.

---
+++ src/com/craft4plus/afksystem/AFKSystem.java
@@ -0,0 +1,15 @@
+package com.craft4plus.afksystem;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+
+public class AFKSystem extends main implements Listener {
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (label.equalsIgnoreCase("afk")) {
+			    if (args.length == 2) {
+				if (sender.hasPermission("c4p.ranks.manage")) {}
+				
+    
+
