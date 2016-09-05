b4523c5fb15445adb905a1aa76f934f554053867
UnitedGamersMC <unitedgamersmc@gmail.com>
2016-09-05 11:47:38 -0500

    More Progress In AFKSystem

--- src/com/craft4plus/afksystem/AFKSystem.java
+++ src/com/craft4plus/afksystem/AFKSystem.java
@@ -1,15 +1,23 @@
 package com.craft4plus.afksystem;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.event.Listener;
+import org.bukkit.entity.Player;
 
-public class AFKSystem extends main implements Listener {
+public class AFKSystem implements CommandExecutor {
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (label.equalsIgnoreCase("afk")) {
-			    if (args.length == 2) {
-				if (sender.hasPermission("c4p.command.afk")) {}
+		} if (!(sender instanceof Player)) {}
+		    sender.sendMessage(ChatColor.RED + "You must be a VIP or higher to use this command!");
+		      return false;
+	}
+	     Player player = (Player) sender;
+	     
+	     return false;
+}}
 				
     
 
