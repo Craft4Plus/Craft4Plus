3570f038fcce7135011f2d426b679144ed682b80
chrismin13 <chrismin13@gmail.com>
2016-09-10 00:27:54 +0300

    Made 1.8 pvp applicable to the intirety of the spawn world

--- src/com/craft4plus/minigames/kitpvp/KitPvPPlayerDetection.java
+++ src/com/craft4plus/minigames/kitpvp/KitPvPPlayerDetection.java
@@ -2,12 +2,10 @@ package com.craft4plus.minigames.kitpvp;
 
 import org.bukkit.entity.Player;
 
-import com.craft4plus.worldguard.PlayerInRegion;
-
 public class KitPvPPlayerDetection {
 
 	public static boolean InSGGame(Player player){ // Boolean that we can access from any class as long as you specify player.
-		if ((PlayerInRegion.InWorldGuardRegion(player, "Spawn", "spawn")) || (PlayerInRegion.InWorldGuardRegion(player, "Spawn", "spawnpvp"))) {
+		if (player.getWorld().getName().equals("Spawn")) {
 			return true;
 		}
 		return false;
