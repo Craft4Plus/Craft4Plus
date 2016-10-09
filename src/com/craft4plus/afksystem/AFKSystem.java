b39a94b45e34f01af83179db29b0e2312ac94e77
chrismin13 <chrismin1302@gmail.com>
2016-10-09 12:14:22 +0300

    Tidied up the code a bit and made the restore gamemode function work

--- src/com/craft4plus/afksystem/AFKSystem.java
+++ src/com/craft4plus/afksystem/AFKSystem.java
@@ -20,58 +20,50 @@ public class AFKSystem implements Listener {
 	HashMap<String, GameMode> GameModeBeforeAFK = new HashMap<String, GameMode>();
 	
 	@EventHandler (priority = EventPriority.HIGHEST)
-	public boolean onAfk(AfkStatusChangeEvent event) {
+	public void onAfk(AfkStatusChangeEvent event) {
 		
 		IUser iuser = event.getAffected(); // User from essentials
 		Player player = Bukkit.getServer().getPlayer(iuser.getName()); // Player
 		if (player.hasPermission("c4p.afk.elite")) { //Just a permission to prevent everyone from having this
 			
 			if (!iuser.isAfk()) { // Weird how this works, this is what happens if the player was not afk and just became afk
-				LocationBeforeAFK.put(player.getName(), player.getLocation()); // Get the location of the player to store it for later
-				GameModeBeforeAFK.put(player.getName(), player.getGameMode()); // Get the GameMode of the player to store it for later
-				
-				player.teleport(new Location(Bukkit.getWorld("BuildWorld"), 2000, 91, 2000)); // Teleport him to the AFK area
-				return true;
-				
+				addToAFKList(player, new Location(Bukkit.getWorld("BuildWorld"), 2000.5, 91, 2000.5));				
 			} else if (iuser.isAfk()) { // Player stopped being AFK
-				
-				if ((LocationBeforeAFK.containsKey(player.getName())) && (GameModeBeforeAFK.containsKey(player.getName()))) { // If we've got somewhere to lead this guy back to
-					Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), new Runnable() { //Schedule a repeating task
-					@Override
-					public void run() {
-						player.teleport(LocationBeforeAFK.get(player.getName())); // Get that location and telport him
-						LocationBeforeAFK.remove(player.getName()); // Remove the location data
-						player.setGameMode(GameModeBeforeAFK.get(player.getName())); // Get his previous gamemode
-						GameModeBeforeAFK.remove(player.getName()); // Remove the location data
-					}
-				}, 1L);
-				}
-				return true;
+				sendUserBack(player);
 			}
 		} else {
 			if (!iuser.isAfk()) { // Weird how this works, this is what happens if the player was not afk and just became afk
-				LocationBeforeAFK.put(player.getName(), player.getLocation()); // Get the location of the player to store it for later
-				GameModeBeforeAFK.put(player.getName(), player.getGameMode()); // Get the GameMode of the player to store it for later
-				
-				player.teleport(new Location(Bukkit.getWorld("BuildWorld"), 100000, 70, 10000)); // Teleport him to the AFK area
-				return true;
-				
+				addToAFKList(player, new Location(Bukkit.getWorld("BuildWorld"), 100000.5, 70, 10000.5));			
 			} else if (iuser.isAfk()) { // Player stopped being AFK
-				
-				if ((LocationBeforeAFK.containsKey(player.getName())) && (GameModeBeforeAFK.containsKey(player.getName()))) { // If we've got somewhere to lead this guy back to
-					Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), new Runnable() { //Schedule a repeating task
-						@Override
-						public void run() {
-							player.teleport(LocationBeforeAFK.get(player.getName())); // Get that location and telport him
-							LocationBeforeAFK.remove(player.getName()); // Remove the location data
-							player.setGameMode(GameModeBeforeAFK.get(player.getName())); // Get his previous gamemode
-							GameModeBeforeAFK.remove(player.getName()); // Remove the location data
-						}
-					}, 1L);
-				}
-				return true;
+				sendUserBack(player);
 			}
 		}
-		return false;
 	}
+	
+	public void addToAFKList(Player player, Location location) {
+		LocationBeforeAFK.put(player.getName(), player.getLocation()); // Get the location of the player to store it for later
+		GameModeBeforeAFK.put(player.getName(), player.getGameMode()); // Get the GameMode of the player to store it for later
+		player.teleport(location); // Teleport him to the AFK area
+	}
+	
+	public void sendUserBack(Player player) {
+		
+		if ((LocationBeforeAFK.containsKey(player.getName())) && (GameModeBeforeAFK.containsKey(player.getName()))) { // If we've got somewhere to lead this guy back to
+			Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), new Runnable() { //Schedule a repeating task
+				@Override
+				public void run() {
+					player.teleport(LocationBeforeAFK.get(player.getName())); // Get that location and telport him
+					LocationBeforeAFK.remove(player.getName()); // Remove the location data
+				}
+			}, 1L);
+			Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), new Runnable() { //Schedule a repeating task
+			@Override
+			public void run() {
+				player.setGameMode(GameModeBeforeAFK.get(player.getName())); // Get his previous gamemode
+				GameModeBeforeAFK.remove(player.getName()); // Remove the location data
+			}
+		}, 3L);
+		}
+	}
+	
 }
